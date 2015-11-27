37f0186020af906ae83a1437f2158474f6f07fce
jhonatancrizpereira <jhonatancrizpereira@gmail.com>
2015-11-27 07:26:12 -0200

    Atualização de arquivos com os do professor Tarcísio

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # senaibrusque
-Situação de aprendizagem - curso de programacao web SENAI
+Situação de Aprendizagem - Curso de Programação Web - GeraçãoTec SENAI/Brusque
+
+#Aulas práticas
+
+##12/11 aula3 - Github
+[Descrição do projeto](https://docs.google.com/document/d/1ILoi1adcFNZJJRTS3HcWRQA-E86mhL_R94thWzFOyrU/edit?usp=sharing)
+
+Descriação do projeto no Github, imlestones e issues
+
+##13/11 aul04 - Bootstrap
+[Design](https://docs.google.com/document/d/198fIKQSZlR00KMI_GhmOJODhFQssMGYfO9iI2Jdw7Hk/edit?usp=sharing)
+
+Configuração do ambiente bootstrap, criação do topo do site.
+
+##16/11 aula05 - Bootstrap
+[Design](https://docs.google.com/document/d/1gHmqwaWCKOmXeue6j2E1xh9uYp4oTIVIBmaedpe9QJ0/edit?usp=sharing)
+
+Ajustes no topo, barra de navegação afixada ao topo após rolagem, rolagem de página suave e carrossel de imagens.
+
+##17/11 aula06 - Bootstrap
+[Design - Primeira entrega parcial](https://docs.google.com/document/d/1452Qxog9UFhh2J0N96tP4js_AxtiO0SEOxo8941sI4s/edit?usp=sharing)
+
+Finalização das issues pendentes de design.
+
+#Avaliação
+* 30% Github completo - Issues sendo criadas e fechadas antes da data de realização.
+* 70% O projeto hospedado no Hostinger com todas funcionalidades
--- index.php
+++ index.php
@@ -4,32 +4,169 @@
         <meta charset="UTF-8">
         <title>Site Institucional do SENAI - Brusque</title>
 
-        <link rel="stylesheet" href="css/bootstrap.min.css"/>
-        <link rel="stylesheet" href="css/estilo.css"/>
+        <!-- Fontes -->
+        <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
 
-        <!-- javascript -->
-        <script src="js/jquery-1.11.3.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/meu_script.js"></script>
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/animate.css" />
+        <link rel="stylesheet" href="css/estilo.css" />
+
 
     </head>
-    <body>
-        <div class="container-fluid">
-            <header class="jumbotron text-center" id="topo">
-                <h1>SENAI/SC Brusque</h1>
-                <p>Ola mundo do bootstrap</p>
-                <form role="form">
-                    <div class="form-inline">
-                        <div class="input-group input-group-sm">
-                            <span class="input-group-addon">
-                                <span class="glyphicon glyphicon-envelope"></span>
-                            </span>
-                            <input type="email" class="form-control" id="email" placeholder="nome@dominio.com.br">
+
+    <body data-spy="scroll" data-target=".navbar" data-offset="50">
+
+        <header class="jumbotron text-center" id="topo">
+            <h1>SENAI/SC Brusque</h1>
+            <p>Alo Ha mundo do Bootstrap</p>
+
+            <form role="form" class="form-inline">
+                <!-- Gerador de Bootstrap Form 
+                     http://embed.plnkr.co/ZrnnkFR3Yv9HGQHrRMiW/preview -->
+                <!-- Em caso de sucesso na validação: <div class="form-group has-feedback has-success"> -->
+                <!-- Em caso de falha na validação: <div class="form-group has-feedback has-error"> -->
+                <div class="form-group has-feedback">
+                    <input type="text" class="form-control" placeholder="seunome@provedor.com.br" size="40px"/>
+                    <i class="form-control-feedback glyphicon glyphicon-envelope"></i>
+                </div>
+                <button type="submit" class="btn btn-success">Cadastrar</button>
+            </form>
+        </header>
+
+        <!-- Barra de Navegação -->
+
+        <nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="250">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>                        
+                    </button>
+                    <a class="navbar-brand" href="#">SENAI</a>
+                </div>
+                <div>
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                        <ul class="nav navbar-nav">
+                            <li><a href="#inicio">Início</a></li>
+                            <li><a href="#cursos">Cursos</a></li>
+                            <li><a href="#noticias">Notícias</a></li>
+                            <li><a href="#fotos">Fotos</a></li>
+                            <li><a href="#videos">Vídeos</a></li>
+                            <li><a href="#contato">Contato</a></li>
+                            <li><a href="#local">Localização</a></li>
+
+                            <!-- Menu drop-down
+                            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Section 4 <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#section41">Section 4-1</a></li>
+                                    <li><a href="#section42">Section 4-2</a></li>
+                                </ul>
+                            </li>
+                            -->
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>    
+        <!-- Fim da Barra de Navegação -->
+
+        <section id="inicio" class="container-fluid">
+
+            <!-- Carrossel de imagens -->
+            <div clas="row">                
+                <!--
+                Uma linha dividida em 12 colunas, sendo uma coluna central
+                tamanho 8 e um distanciamento de 2
+                
+                Sistema de Grid no Bootstrap
+                http://www.w3schools.com/bootstrap/bootstrap_grid_examples.asp
+                -->
+                <div class="col-lg-8 col-lg-offset-2">
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel" data-pause="hover" data-interval="2000">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel" data-slide-to="1"></li>
+                            <li data-target="#myCarousel" data-slide-to="2"></li>
+                            <li data-target="#myCarousel" data-slide-to="3"></li>
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+                            <div class="item active">
+                                <img src="img/saiba_mais_sobre_o_senai.png" alt="Chania">
+                            </div>
+
+                            <div class="item">
+                                <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=460%C3%97365&w=460&h=365" alt="Chania">
+                            </div>
+
+                            <div class="item">
+                                <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=460%C3%97365&w=460&h=365" alt="Flower">
+                            </div>
+
+                            <div class="item">
+                                <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=460%C3%97365&w=460&h=365" alt="Flower">
+                            </div>
                         </div>
-                    </div>                    
-                    <button type="submit" class="btn btn-success">Cadastrar</button>
-                </form>
-            </header>
-        </div>
+
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+            </div> <!-- row -->
+            <!-- fim do carrossel de imagens -->
+        </section>
+
+        <section id="cursos" class="container-fluid">
+            <h1>Cursos</h1>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        </section>
+
+        <section id="noticias" class="container-fluid">
+            <h1>Notícias</h1>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        </section>
+
+        <section id="fotos" class="container-fluid">
+            <h1>Fotos</h1>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        </section>
+
+        <section id="videos" class="container-fluid">
+            <div class="text-center">
+                <h1>Galeria Vídeos</h1>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLXTxMOByxQKOZcxIvbgx_93ItPjnbqYIk" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </section>
+
+        <?php include 'formContato.php'; ?>
+
+        <section id="local" class="container-fluid">
+            <div class="text-center">
+                <h1>Localização</h1>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d5972.578301415663!2d-48.91459628134629!3d-27.11484556231412!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x94df47b2050e1221%3A0xb0ebeaaf853b447!2sAv.+Primeiro+de+Maio%2C+670+-+Primeiro+de+Maio%2C+Brusque+-+SC%2C+88353-202!3m2!1d-27.1156975!2d-48.910202!5e0!3m2!1spt-BR!2sbr!4v1447800510027" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+        </section>
+
+        <!-- Javascript -->
+        <script src="js/jquery-1.11.3.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/validator.js"></script>
+        <script src="js/meuscript.js"></script>
+        <script src="js/formScript.js"></script>
+
     </body>
 </html>
