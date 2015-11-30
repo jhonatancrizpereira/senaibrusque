c84779eeb0c8456c367125dac5218753cf14c990
jhonatancrizpereira <jhonatancrizpereira@gmail.com>
2015-11-30 14:28:50 -0200

    Cadastro na newsletter

--- formNewsletter.php
+++ formNewsletter.php
@@ -1,13 +1,21 @@
-<form action="newsletter/newsletter.php" method="post" class="form-inline" >
+<div id="msgCadastroSubmit" class="h3 text-center hidden"></div>
+
+<form role="form" class="form-inline" id='formCadastro'>
     <!-- Gerador de Bootstrap Form 
          http://embed.plnkr.co/ZrnnkFR3Yv9HGQHrRMiW/preview -->
     <!-- Em caso de sucesso na validação: <div class="form-group has-feedback has-success"> -->
     <!-- Em caso de falha na validação: <div class="form-group has-feedback has-error"> -->
+
+    <!-- Erro de validação -->
+    <div class="help-block with-errors"></div>
+    <!-- Sucessso ou falha no cadastro --> 
+    
+
     <div class="form-group has-feedback">
-        <input type="email" name="email" required class="form-control" placeholder="seunome@provedor.com.br" size="40px"/>
+        <input id="cadastroEmail" type="text" class="form-control" placeholder="seunome@provedor.com.br" size="40px" required/>
         <i class="form-control-feedback glyphicon glyphicon-envelope"></i>
-        <div class="help-block with-errors"></div>
     </div>
-    <input type="submit" name="btn" class="btn btn-success"/>
-    <div id="msgSubmit" class="h3 text-center hidden"></div>
+    <button id="cadastroSubmit" type="submit" class="btn btn-success">Cadastrar</button>
+
+
 </form>
